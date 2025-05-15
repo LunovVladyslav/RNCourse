1d9bc301259f5fcbe954aeef2f8be4558acf1894
Vladyslav Lunov <vladyslavlunov@gmail.com>
2025-05-15 22:49:30 +0200

    added styles

--- App.js
+++ App.js
@@ -10,9 +10,8 @@ export default function App() {
         <TextInput placeholder='Your course goal!' style={styles.textInput} />
         <Button title='Add Goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
-        <Text>List of Balls...</Text>
       </View>
     </View>
   );
@@ -22,15 +21,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
-    backgroundColor: '#fff',
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 24,
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 2,
   },
   textInput: {
-    borderColor: '#2B60FFFF',
+    borderColor: '#cccccc',
     borderWidth: 1,
     borderRadius: 6,
     width: '70%',
@@ -39,4 +42,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     width: '20%',
   },
+  goalsContainer: {
+    flex: 5,
+  }
 });
